9460b1953ef753f52f9fac00c3c8259cb65f44da
stef663k <stef663k@elev.techcollege.dk>
2025-04-10 12:15:34 +0200

    bvcxx

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -32,8 +32,7 @@
     </div>
     <div class="aboutme">
         <p>
-            I'm a 36 year old guy, who decided to switch day job out with the books during a pandemic. 
-            I started learning c# last january, and I am currently a beginner in this language. 
+            I'm a 36 year old guy, who decided to switch day job out with the books during a pandemic. I started learning c# and,
             I got a couple of projects under the project tap, where you can see them on github. 
             There will come more projects on there as i make them. 
             I also started dabling in Zig and Unity. 
